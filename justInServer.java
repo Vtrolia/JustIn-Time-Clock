28ba5ebc053bc45b56111dab5a96e5af937876e4
gallimania <38448592+gallimania@users.noreply.github.com>
2018-12-27 00:23:37 -0600

    Basic server class and methods

--- justInServer.java
+++ justInServer.java
@@ -1,3 +1,37 @@
+import java.io.IOException;
+import java.net.*;
+import java.sql.*;
+
 public class justInServer {
-//  todo
+
+
+}
+
+class JustInServerSocket   {
+
+    private int portNum;
+    private ServerSocket ourSocket;
+
+    public JustInServerSocket(int port) {
+        portNum = port;
+        try {
+            ourSocket = new ServerSocket(portNum);
+        }
+        catch (UnknownHostException e) {
+            return;
+        }
+        catch (IOException ioe) {
+            return;
+        }
+
+
+        try {
+            ourSocket.accept();
+        }
+        catch (IOException ioe) {
+            return;
+        }
+
+
+    }
 }
